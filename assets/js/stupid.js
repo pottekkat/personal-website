1d49fa4777bbf18f0ede7ebfb4482f7ee2119bf1
Navendu Pottekkat <navendu@apache.org>
2024-05-22 19:56:28 +0530

    fix art
    Signed-off-by: Navendu Pottekkat <navendu@apache.org>

--- assets/js/stupid.js
+++ assets/js/stupid.js
@@ -1,1 +1,11 @@
-console.log("\r\n   _  __                      __       ___       __  __      __    __        __ \r\n  \/ |\/ \/__ __  _____ ___  ___\/ \/_ __  \/ _ \\___  \/ \/_\/ \/____ \/ \/__ \/ \/_____ _\/ \/_\r\n \/    \/ _ `\/ |\/ \/ -_) _ \\\/ _  \/ \/\/ \/ \/ ___\/ _ \\\/ __\/ __\/ -_)  \'_\/\/  \'_\/ _ `\/ __\/\r\n\/_\/|_\/\\_,_\/|___\/\\__\/_\/\/_\/\\_,_\/\\_,_\/ \/_\/   \\___\/\\__\/\\__\/\\__\/_\/\\_\\\/_\/\\_\\\\_,_\/\\__\/\n\nhttps://github.com/pottekkat/personal-website");
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+if (!isSafari) {
+  console.log(
+    '\r\n88b 88    db    Yb    dP 888888 88b 88 8888b.  88   88     88""Yb  dP"Yb  888888 888888 888888 88  dP 88  dP    db    888888 \r\n88Yb88   dPYb    Yb  dP  88__   88Yb88  8I  Yb 88   88     88__dP dP   Yb   88     88   88__   88odP  88odP    dPYb     88   \r\n88 Y88  dP__Yb    YbdP   88""   88 Y88  8I  dY Y8   8P     88"""  Yb   dP   88     88   88""   88"Yb  88"Yb   dP__Yb    88   \r\n88  Y8 dP""""Yb    YP    888888 88  Y8 8888Y"  `YbodP\'     88      YbodP    88     88   888888 88  Yb 88  Yb dP""""Yb   88\n\nhttps://github.com/pottekkat/personal-website'
+  );
+} else {
+    console.log(
+        'I hate Safari. I had cool ASCII art here, but Safari isn\'t cool enough to display it correctly.\n\nNavendu Pottekkat\n\nhttps://github.com/pottekkat/personal-website'
+    )   
+}
